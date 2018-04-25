fa3fd8f2a3c013f9198da5ed70c8bf73c22e5c47
TzachBen <tzach_ben@hotmail.com>
2018-04-25 15:47:16 +0300

    commit

---
+++ socialWorkOut/config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+    'url' : 'mongodb://<dbuser>:<dbpassword>@ds036617.mlab.com:36617/socialworkout' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+     
+     //Please replace your host file Here : 127.1.1.0 , Express is Collection Name (Database Name)
+};
